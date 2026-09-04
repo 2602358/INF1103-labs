d0037a6915516300d079595b11931ddaa370fbbb
Felix Ng <2602358@sit.singaporetech.edu.sg>
2026-09-04 15:21:29 +0800

    Fourth Post

--- hello.py
+++ hello.py
@@ -1,21 +1,31 @@
-print("==================")
-print("Welcome here")
-print("My first post!!!")
-print("==================")
+# print("==================")
+# print("Welcome here")
+# print("My first post!!!")
+# print("==================")
 
-username = "Cool_Creator"
-bio = "Fun Blogger"
-followers = 100
+# username = "Cool_Creator"
+# bio = "Fun Blogger"
+# followers = 100
 
+# print("Username:", username)
+# print("bio:", bio)
+# print("followers:", followers)
+
+# followers += 50
+# print("Day1:", followers)
+
+# followers += 20
+# print("Day2:", followers)
+
+# followers -= 10
+# print("Day3:", followers)
+
+username = input("Enter Username: ")
+age = input("Enter age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Prolie")
+print("===================")
 print("Username:", username)
-print("bio:", bio)
-print("followers:", followers)
-
-followers += 50
-print("Day1:", followers)
-
-followers += 20
-print("Day2:", followers)
-
-followers -= 10
-print("Day3:", followers)
+print("Age:", age)
+print ("Category:", category)
