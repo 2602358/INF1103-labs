34f44539e521e258a7e63d9c126fae285a077327
Felix Ng <2602358@sit.singaporetech.edu.sg>
2026-09-11 04:48:19 +0800

    Lab 2 Fifth Post

--- auditor.py
+++ auditor.py
@@ -1,13 +1,16 @@
-inventory = 0
+inventory = 0 #Initialize the inventory to zero in the start
 
-while True:
+while True: #Run in a continuous loop asking user to enter a stock quantity.
     stock_quantity = (input("Enter Stock Quantity (Enter 'quit' to quit): "))
-    if stock_quantity.lower() == "quit":
-        print("Quit. Total inventory:", inventory)
+    if stock_quantity.lower() == "quit": #Until the user types quit.
+        print("Quit. Total inventory:", inventory) #Reporting
         break
-    elif stock_quantity.isdigit():
-        stock_value = int(stock_quantity)
-        inventory += stock_value
+    elif stock_quantity.isdigit(): #Handle invalid input
+        stock_value = int(stock_quantity) #Accept stock values as integers
+        inventory += stock_value #Manage State
         print ("Inventory:",inventory)
-    else:
+    elif not stock_quantity.isdigit():
+        if stock_quantity.startswith("-"): #Enforce business rules
+            print ("Negative numbers rejected. Please enter a valid number")
+        else:
             print("Error. Please enter a valid number")
